8c157ff820d02fdb248252859d7efd739cccbd32
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2026-03-10 08:40:31 +0700

    #

--- goland/arrays/for_loop.go
+++ goland/arrays/for_loop.go
@@ -5,22 +5,22 @@ import "fmt"
 /*
 	- Chỉ có 1 loại duy nhât là for loop, nhưng có 3 cách viết khác nhau:
 
-	1. Cách 1: Cú pháp giống C, Java, Python
-		for i := 0; i < 5; i++ {
-			fmt.Println(i)
-		}
+		Cách 1: Cú pháp giống C, Java, Python
+			for i := 0; i < 5; i++ {
+				fmt.Println(i)
+			}
 
-	2. Cách 2: Chỉ có điều kiện, không có phần khởi tạo và bước nhảy
-		i := 0
-		for i < 5 {
-			fmt.Println(i)
-			i++
-		}
+		Cách 2: Chỉ có điều kiện, không có phần khởi tạo và bước nhảy
+			i := 0
+			for i < 5 {
+				fmt.Println(i)
+				i++
+			}
 
-	3. Cách 3: Vòng lặp vô hạn (infinite loop)
-		for {
-			fmt.Println("Hello")
-		}
+		Cách 3: Vòng lặp vô hạn (infinite loop)
+			for {
+				fmt.Println("Hello")
+			}
 */
 
 func loop1() {
