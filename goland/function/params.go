33963a804a6380f82e728bab774ced57dfe09de5
nguyenhuyenag <42534837+nguyenhuyenag@users.noreply.github.com>
2026-03-10 16:20:57 +0700

    #

---
+++ goland/function/params.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+	func add(a int, b int, c int) int {
+
+	}
+
+	Tương đương với:
+
+	func add(a, b, c int) int {
+
+	}
+*/
+func main() {
+
+}
---
+++ goland/str/unicode.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "abcd"
+	r := []rune(s) // Convert to unicode array: [97, 98, 99, 100]
+	chars := []byte(s)
+	fmt.Println(r)
+	fmt.Println(chars)
+	fmt.Println(string(r))
+	fmt.Println(string(chars))
+}
